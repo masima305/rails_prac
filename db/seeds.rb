82bbf8bc37719c1413440025d88d93bb57ee6d25
masima305 <masima305@gmail.com>
2017-02-19 02:51:45 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recip.create(ing_1:"코코아", ing_2:"바닐라",ing_3:"휘핑",ing_l:"버섯수프")
+Recip.create(ing_1:"딸기", ing_2:"생크림",ing_3:"모과",ing_l:"계란노른자")
+Recip.create(ing_1:"잦", ing_2:"밥알",ing_3:"단술",ing_l:"계피")
+#명령어 " rake db:seed"
